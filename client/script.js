e836958665fdeff25a23f6c022ce9e2e131d8b82
Andrew H <aho338@gmail.com>
2016-07-31 22:37:51 -0700

    Add front-end logic

---
+++ client/script.js
@@ -0,0 +1,44 @@
+var jobId = null;
+
+var updateJobId = function(id) {
+  jobId = id;
+  $('.job-id').html(jobId);
+  $('.job-id-display').show();
+};
+
+var updateJobStatus = function(status) {
+  $('.status').html(status);
+};
+
+var initialize = function() {
+  $('.job-id-display').hide();
+};
+
+$('.url-submit').click(function() {
+  var url = $('.url-field').val();
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:8080/url',
+    data: {
+      url: url
+    },
+    success: function(data) {
+      updateJobId(data);
+    }
+  });
+});
+
+$('.status-submit').click(function() {
+  var jobId = $('.status-field').val();
+  if (jobId) {
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:8080/job/' + jobId,
+      success: function(data) {
+        updateJobStatus(data);
+      }
+    });
+  }
+});
+
+initialize();
